a94f013493fbf56e132a4dee67b48f1597b6844c
Cristi <cristy11_1998@yahoo.com>
2020-01-13 19:18:08 +0200

    added bind events function for shop page

--- js/shop.js
+++ js/shop.js
@@ -13,11 +13,30 @@ window.Shop = {
 
     },
 
+    addProductToCart: function (productId){
+        var request = {
+            //todo: take customer id dynamically somehow
+            customerId: 3,
+            productId: productId
+        };
+
+        $.ajax({
+            url: Shop.API_BASE_URL + "/carts",
+            method: "PUT",
+            contentType: "application/json",
+            data: JSON.stringify(request)
+        }).done(function () {
+            window.location.replace("cart.html");
+
+        })
+
+    },
+
     getProductHtml: function (product) {
         return `<div class="col-md-3 col-sm-6">
                     <div class="single-shop-product">
                         <div class="product-upper">
-                            <img src="img/product-2.jpg" alt="">
+                            <img src="img/banana2.jpg" alt="">
                         </div>
                         <h2><a href="">${product.name}</a></h2>
                         <div class="product-carousel-price">
@@ -37,7 +56,18 @@ window.Shop = {
         products.forEach(oneProduct => productsHtml += Shop.getProductHtml(oneProduct));
 
         $(".single-product-area .row:first-child").html(productsHtml);
+    },
+
+    bindEvents: function () {
+        $(".single-product-area").delegate(".add_to_cart_button", "click", function (event) {
+            event.preventDefault();
+
+            let productId = $(this).data("product_id");
+
+            Shop.addProductToCart(productId);
+        })
     }
 };
 
 Shop.getProducts();
+Shop.bindEvents();
